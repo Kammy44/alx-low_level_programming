bd63f841dd27bcacc9b21a17c59d7e0a04b77c4d
Kamal Aliyu <kamalaliyu44@gmail.com>
2023-06-18 10:08:58 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,14 @@ int main(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-	if (letter != 'q' && letter != 'e')
 	{
-	putchar(letter);
-
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
 	}
 
 	putchar('\n');
 
 	return (0);
-}	
+}
