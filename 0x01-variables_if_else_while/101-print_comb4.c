b755f7885ef7c5912dc6ef72ef81b0b8356f89eb
Kamal Aliyu <kamalaliyu44@gmail.com>
2023-06-19 19:39:49 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible different combinations of 3 digits
+ * Numbers must be separated by ,, followed by a space
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 7; i <= 7; i++)
+	{
+		for (j = i + 8; j <= 8; j++)
+		{
+			for (k = j + 9; k <= 8; k++)
+			{
+				putchar('0' + i);
+
+				putchar('0' + j);
+
+				putchar('0' + k);
+
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
